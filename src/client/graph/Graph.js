0c3979c95d9d2b9c199635d24c098ae8161fc807
Nehal Parimi <parimi.nehal@gmail.com>
2020-08-27 16:45:45 -0700

    Create graph and 2d array.

--- src/client/graph/Graph.js
+++ src/client/graph/Graph.js
@@ -9,27 +9,89 @@ class Graph extends Component {
         this.state = {
             graph: []
         }
+        this.generateGraph = this.generateGraph.bind(this);
+        this.bfs = this.bfs.bind(this); 
     }
 
     generateGraph() {
-        let arr = [];
-        for (let i = 0; i < 100; i++) {
-            let color = Math.random() > 0.5 ? "white" : "black";
-            arr.push(<Vertex color={color}/>);
-            
+        let graph = [];
+        for (let i = 0; i < 10; i++) {
+            let arr = [];
+            for (let j = 0; j < 30; ++j) {
+                arr.push(<Vertex key={i + j} x={i} y={j} className='unvisited' />); 
+            }
+            graph.push(arr);
         }
-        this.setState({
-            graph: arr,
-        });
+        this.setState({ graph });
     }
 
     componentDidMount() {
         this.generateGraph();
     }
+
+    bfs(startX, startY, endX, endY) {
+        let { graph } = this.state;
+        let visited = [];
+
+        for (let i = 0; i < graph.length; ++i) {
+            let arr = [];
+
+            for (let j = 0; j < graph[i].length; ++j) {
+                arr.push(0);
+            }
+            visited.push(arr);
+        }
+
+        function checkIfValid(x, y) {
+            return x >= 0 && x < graph.length
+                && y >= 0 && y < graph[x].length
+                && !visited[x][y]
+        } 
+
+        let start = graph[startX][startY];
+
+        let queue = [];
+        queue.push(start);
+        visited[startX][startY] = 1;
+     
+        let queueIter = 0;
+
+        while (queueIter < queue.length) {
+            let current = queue[queueIter];
+            let { x, y } = current.props;
+            let dupGraph = graph;
+
+            // if (x == endX && y === endY) return true;
+
+
+            let shift = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+            for (let arr of shift) {
+                let xCoor = x + arr[0];
+                let yCoor = y + arr[1];
+                if (checkIfValid(xCoor, yCoor)) {
+                    let validNeighbor = graph[xCoor][yCoor];
+                    visited[xCoor][yCoor] = 1;
+                    if (validNeighbor.props.x == endX && validNeighbor.props.y == endY) return true;
+                    queue.push(validNeighbor);
+                }
+            }
+            console.log(dupGraph[x][y].props.className);
+            
+                this.setState({
+                    graph: dupGraph,
+                });
+            
+            queueIter++;
+        }
+        return false;
+    }
+
     render() {
+        const { graph } = this.state;
         return (
             <div className="graph">
-                {this.state.graph}
+                { graph }
+                <button className="runButton" onClick={() => this.bfs(0, 0, 5, 6)}>Run BFS</button>
             </div>
             
         )
--- src/client/graph/Vertex.js
+++ src/client/graph/Vertex.js
@@ -4,34 +4,18 @@ import './Vertex.css'
 class Vertex extends Component {
     constructor(props) {
         super(props);
-        
+
         this.state = {
-            backgroundColor: this.props.color,
-            border: "1px solid black"
-        }
-
-        this.toggleColor = this.toggleColor.bind(this);
-    }
-
-    toggleColor() {
-        let color = this.state.backgroundColor;
-
-        if (color == "white") {
-            this.setState({
-                backgroundColor: "black",
-            });
-        }
-        else {
-            this.setState({
-                backgroundColor: "white",
-            });
+            classname: this.props.className
         }
     }
 
-
+    componentDidUpdate() {
+        console.log(this.props.className)
+    }
 
     render() {
-        return <div className="node" style={this.state} onClick={this.toggleColor}></div>
+        return <div style={{ border: "1px solid black" }} className={this.state.classname}></div>
     }
     
 }
